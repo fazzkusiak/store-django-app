7078c8909f111764c1ee15eac4aae6cbda524f0d
fazzkusiak <sactarek@gmail.com>
2022-12-29 11:20:29 +0100

    defined database

---
+++ shop/models.py
@@ -0,0 +1,40 @@
+from django.db import models
+
+class Product(models.Model):
+    title = models.CharField(max_length=255)
+    description = models.TextField()
+    price = models.DecimalField(max_digits=6, decimal_places=2)
+    inventory = models.IntegerField()
+    last_update = models.DateTimeField(auto_now=True)
+
+
+class Customer(models.Model):
+    MEMBERSHIP_BRONZE = "B"
+    MEMBERSHIP_SILVER = "S"
+    MEMBERSHIP_GOLD = "G"
+
+    MEMBERSHIP_CHOICES = [
+        (MEMBERSHIP_BRONZE, 'Bronze'),
+        (MEMBERSHIP_SILVER, 'Silver'),
+        (MEMBERSHIP_GOLD, 'Gold'),
+    ]
+    first_name = models.CharField(max_length=25)
+    last_name = models.CharField(max_length=30)
+    email = models.EmailField(unique=True)
+    phone = models.CharField(max_length=11)
+    birth_date = models.DateField(null=True)
+    membership = models.CharField(max_length=1, choices=MEMBERSHIP_CHOICES, default=MEMBERSHIP_BRONZE)
+
+
+class Order(models.Model):
+    PAYMENT_PENDING = "P"
+    PAYMENT_COMPLETE = "C"
+    PAYMENT_FAILED = "F"
+
+    PAYMENT_CHOICES = [
+        (PAYMENT_PENDING, 'Pending'),
+        (PAYMENT_COMPLETE, 'Complete'),
+        (PAYMENT_FAILED, 'Failed'),
+    ]
+    placed_at = models.DateTimeField(auto_now_add=True)
+    payment_status = models.CharField(max_length=1, choices=PAYMENT_CHOICES, default=PAYMENT_PENDING)
