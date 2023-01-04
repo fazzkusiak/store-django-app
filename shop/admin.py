ff947d56387611f496b77a84c43cf191b9e7c720
kubusiaq <sactarek@gmail.com>
2023-01-04 21:00:05 +0100

    added admin pages to Collection, Customer, Order and Product

--- shop/admin.py
+++ shop/admin.py
@@ -1,3 +1,43 @@
 from django.contrib import admin
+from . import models
+
+@admin.register(models.Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ['title', 'unit_price', 'inventory_status', 'collection_title']
+    list_editable = ['unit_price']
+    list_per_page = 10
+    list_select_related = ['collection']
+
+    def collection_title(self, product):
+        return product.collection.title
+
+    @admin.display(ordering='inventory')
+    def inventory_status(self, product):
+        if product.inventory < 10:
+            return 'low'
+        return 'ok'
+
+@admin.register(models.Customer)
+class CustomersAdmin(admin.ModelAdmin):
+    list_display = ['first_name', 'last_name', 'membership']
+    list_editable = ['membership']
+    ordering = ["first_name"]
+    list_per_page = 10
+
+@admin.register(models.Order)
+class OrdersAdmin(admin.ModelAdmin):
+    list_display = ['id', 'placed_at', 'customer']
+    ordering = ['id']
+    list_per_page = 10
+    list_select_related = ['customer']
+
+    
+
+    def customer_first_name(self, order):
+        return order.customer.first_name
+
+    def customer_last_name(self, order):
+        return order.customer.last_name
+admin.site.register(models.Collection)
 
 # Register your models here.
