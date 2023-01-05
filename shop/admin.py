e0a12f5b3f581489ff2099c33674214a7ea17576
fazzkusiak <sactarek@gmail.com>
2023-01-05 23:04:38 +0100

    fixed name displaying

--- shop/admin.py
+++ shop/admin.py
@@ -100,18 +100,13 @@ class CustomerAdmin(admin.ModelAdmin):
 
 @admin.register(models.Order)
 class OrderAdmin(admin.ModelAdmin):
-    list_display = ['id', 'placed_at', 'customer']
+    list_display = ['id', 'placed_at', 'customer_name']
     ordering = ['id']
     list_per_page = 10
     list_select_related = ['customer']
 
-    
-
-    def customer_first_name(self, order):
-        return order.customer.first_name
-
-    def customer_last_name(self, order):
-        return order.customer.last_name
+    def customer_name(self, order):
+        return order.customer.first_name + " " + order.customer.last_name
 
 
 # Register your models here.
--- shop/models.py
+++ shop/models.py
@@ -4,6 +4,9 @@ class Collection(models.Model):
     title = models.CharField(max_length=255)
     featured_product = models.ForeignKey('Product', on_delete=models.SET_NULL, null=True, related_name='+')
 
+    def __str__(self) -> str:
+        return self.title
+
 class Promotion(models.Model):
     discount = models.IntegerField()
     description = models.CharField(max_length=255)
