65a821e221ce275af2a9d6b9bcd6024adb817745
fazzkusiak <sactarek@gmail.com>
2023-01-05 21:22:07 +0100

    added filtering option in products page

--- shop/admin.py
+++ shop/admin.py
@@ -6,11 +6,13 @@ from django.utils.html import format_html, urlencode
 from django.urls import reverse
 from . import models
 
+
 @admin.register(models.Collection)
 class CollectionAdmin(admin.ModelAdmin):
     list_display = ['title', 'products_count']
     list_per_page = 10 
 
+    
     @admin.display(ordering='products_count')
     def products_count(self, collection):
         url = (
@@ -27,7 +29,22 @@ class CollectionAdmin(admin.ModelAdmin):
         return super().get_queryset(request).annotate(
             products_count = Count('product')
         )
+   
 
+class InventoryFilter(admin.SimpleListFilter):
+    title = 'inventory'
+    parameter_name = 'inventory'
+
+    def lookups(self, request, model_admin):
+        return [ 
+            ('<10', 'Low')
+        ]
+
+    def queryset(self, request, queryset):
+        if self.value() == '<10':
+            return queryset.filter(inventory__lte=10)
+
+    
 
 @admin.register(models.Product)
 class ProductAdmin(admin.ModelAdmin):
@@ -35,7 +52,9 @@ class ProductAdmin(admin.ModelAdmin):
     list_editable = ['unit_price']
     list_per_page = 10
     list_select_related = ['collection']
+    list_filter = ['collection', 'last_update', InventoryFilter]
 
+   
     def collection_title(self, product):
         return product.collection.title
 
@@ -51,7 +70,7 @@ class CustomerAdmin(admin.ModelAdmin):
     list_editable = ['membership']
     list_per_page = 10
     search_fields = ['first_name__istartswith', 'last_name__istartswith']
-    
+
     @admin.display(ordering='orders')
     def orders(self, customer):
         # ?customer_id=1
