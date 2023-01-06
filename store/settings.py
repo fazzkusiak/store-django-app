349fb23d4c929b4dfbde62582441db02fa9fdbe6
fazzkusiak <sactarek@gmail.com>
2023-01-06 13:53:30 +0100

    set databases settings

--- store/settings.py
+++ store/settings.py
@@ -82,7 +82,7 @@ WSGI_APPLICATION = 'store.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'store',
+        'NAME': 'store2',
         'HOST': 'localhost',
         'USER': 'root',
         'PASSWORD': 'root'
