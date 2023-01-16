d0b1dae1ff9d59b061fa2b0cdaf47c9301387608
kubusiaq <sactarek@gmail.com>
2023-01-16 14:39:57 +0100

    added serializers, deserializes, meta class, validation of data

--- shop/serializers.py
+++ shop/serializers.py
@@ -1,6 +1,21 @@
 from rest_framework import serializers
+from shop.models import Product, Collection
+from decimal import Decimal
 
-class ProductSerializer(serializers.Serializer):
-    id = serializers.IntegerField()
-    title = serializers.CharField(max_length=255)
-    unit_price = serializers.DecimalField(max_digits=6, decimal_places=2)
+
+class CollectionSerializer(serializers.Serializer):
+    class Meta:
+        model = Collection
+        fields = ['id', 'title']
+
+class ProductSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Product
+        fields = ['id', 'title', 'unit_price', 'price_with_tax', 'collection']
+    
+    price_with_tax = serializers.SerializerMethodField(method_name='calculate_tax')
+    
+    
+
+    def calculate_tax(self, product: Product):
+        return product.unit_price * Decimal(1.1)
--- shop/views.py
+++ shop/views.py
@@ -2,15 +2,29 @@ from django.shortcuts import get_object_or_404
 from django.http import HttpResponse
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
+from rest_framework import status
 from .models import Product
 from .serializers import ProductSerializer
 
-@api_view()
+@api_view(['GET', 'POST'])
 def product_list(request):
-    return Response("ok")
-
+    if request.method == 'GET':
+        queryset = Product.objects.select_related('collection').all()
+        serializer = ProductSerializer(
+            queryset, many=True, context={'request': request}
+            )
+        return Response(serializer.data)
+    elif request.method == 'POST':
+        serializer = ProductSerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        print(serializer.validated_data)
+        return Response('okk')
 @api_view()
 def product_detail(request, id):
     response = get_object_or_404(Product, pk=id)
     serializer = ProductSerializer(response)
     return Response(serializer.data)
+
+@api_view()
+def collection_detail(request, pk):
+    return Response('ok')
