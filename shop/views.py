86873c71884cef9b4a9560fa2b10ac8e68724d76
fazzkusiak <sactarek@gmail.com>
2023-01-20 21:24:31 +0100

    updated post, put requests

--- shop/views.py
+++ shop/views.py
@@ -17,13 +17,21 @@ def product_list(request):
     elif request.method == 'POST':
         serializer = ProductSerializer(data=request.data)
         serializer.is_valid(raise_exception=True)
-        print(serializer.validated_data)
-        return Response('okk')
-@api_view()
+        serializer.save()
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
+        
+@api_view(['GET', 'PUT'])
 def product_detail(request, id):
-    response = get_object_or_404(Product, pk=id)
-    serializer = ProductSerializer(response)
-    return Response(serializer.data)
+    product = get_object_or_404(Product, pk=id)
+    if request.method == "GET":
+        serializer = ProductSerializer(product)
+        return Response(serializer.data)
+    elif request.method == "PUT":
+        serializer = ProductSerializer(product, data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data)
+
 
 @api_view()
 def collection_detail(request, pk):
