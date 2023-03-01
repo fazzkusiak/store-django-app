d74b5b33c3ff39172591531463517a066d1a6743
kubusiaq <sactarek@gmail.com>
2023-03-01 16:56:09 +0100

    refactory product and collection model to class based views

--- shop/urls.py
+++ shop/urls.py
@@ -20,6 +20,6 @@ from . import views
 urlpatterns = [
     path('products/', views.ProductList.as_view()),
     path('products/<int:id>', views.ProductDetail.as_view()),
-    path('collections/<int:id>/', views.collection_detail, name='collection-detail'),
-    path('collections/', views.collection_list)
+    path('collections/<int:id>/', views.CollectionList.as_view() , name='collection-detail'),
+    path('collections/', views.CollectionList.as_view())
 ]
