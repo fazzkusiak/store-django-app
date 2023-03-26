64c7f4ce1de31f32c334e0998df8240c66b1fd57
kubusiaq <sactarek@gmail.com>
2023-03-26 19:18:20 +0200

    added pillow to pipfile

--- shop/models.py
+++ shop/models.py
@@ -32,7 +32,12 @@ class Product(models.Model):
 
     def __str__(self) -> str:
         return self.title
-        
+
+class ProductImage(models.Model):
+    product = models.ForeignKey(Product, on_delete=models.CASCADE, related_name='images')
+    image = models.ImageField(upload_to='shop/images')
+
+
 class Customer(models.Model):
     class Meta:
         permissions = [
