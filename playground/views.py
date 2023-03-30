074911c2047fa1da21e198f8a691b18a3e52e566
kub <sactarek@gmail.com>
2023-03-30 17:42:54 +0200

    created class based view cache method

--- playground/views.py
+++ playground/views.py
@@ -1,18 +1,26 @@
+from django.core.cache import cache
+import requests
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.core.mail import send_mail, mail_admins, BadHeaderError, EmailMessage
 from templated_mail.mail import BaseEmailMessage
 from store.tasks import notify_customers
+from django.views.decorators.cache import cache_page
+from rest_framework.views import APIView
+from django.utils.decorators import method_decorator
 # Create your views here.
+class HelloView(APIView):
+    @method_decorator(cache_page(5 * 60))
+    def get(self, request):
+        response = requests.get('https://httpbin.org/delay/2')
+        data = response.json()
+        return render(request, 'hello.html', {'name' : data})
 
-def calculate():
-    x = 1
-    y = 2
-    return x 
-
-def say_hello(request):
-    
-    return render(request, 'hello.html', {'name' : 'Mosh'})
+# @cache_page(5 * 60)
+# def say_hello(request):
+#     response = requests.get('https://httpbin.org/delay/2')
+#     data = response.json()
+#     return render(request, 'hello.html', {'name' : data})
 
 '''
 def say_hello(request):
