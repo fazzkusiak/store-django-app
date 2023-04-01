e399845cc3e7bc266d81a2fa7a36f76745db9168
kubusiaq <sactarek@gmail.com>
2023-04-01 18:58:46 +0200

    database fixtures

--- shop/urls.py
+++ shop/urls.py
@@ -36,7 +36,7 @@ if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, 
                     document_root=settings.MEDIA_ROOT)
     
-    urlpatterns += [path('silk/', include('silk.urls', namespace='silk'))]
+    #urlpatterns += [path('silk/', include('silk.urls', namespace='silk'))]
 
 
 router = routers.DefaultRouter()
