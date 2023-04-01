e0022db950b3156f4d01b1d613793d9340618e11
kubusiaq <sactarek@gmail.com>
2023-04-01 13:09:57 +0200

    added allowed heroku host

--- store/settings/prod.py
+++ store/settings/prod.py
@@ -6,4 +6,4 @@ SECRET_KEY = os.environ['SECRET_KEY']
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['kubbuy-prod.herokuapp.com']
