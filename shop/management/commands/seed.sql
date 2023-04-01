cb175bae091b3f496ddce887cab52e77e1bb0906
kubusiaq <sactarek@gmail.com>
2023-04-01 19:13:58 +0200

    seed

--- shop/management/commands/seed.sql
+++ shop/management/commands/seed.sql
@@ -1,15 +1,3 @@
-insert into
-  shop_collection (id, title, featured_product_id)
-values
-  (2, 'Grocery', null),
-  (3, 'Beauty', null),
-  (4, 'Cleaning', null),
-  (5, 'Stationary', null),
-  (6, 'Pets', null),
-  (7, 'Baking', null),
-  (8, 'Spices', null),
-  (9, 'Toys', null),
-  (10, 'Magazines', null);
 
 insert into
   shop_product (
