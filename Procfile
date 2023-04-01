e482bd311fb3e5663e1f08e1a7a140e3a171646c
kubusiaq <sactarek@gmail.com>
2023-04-01 13:57:20 +0200

    ..

--- Procfile
+++ Procfile
@@ -1,1 +1,4 @@
-release: python manage.py migrate 
+# Modify this Procfile to fit your needs
+instance: python manage.py migrate
+web: gunicorn store/wsgi
+
--- store/settings/prod.py
+++ store/settings/prod.py
@@ -7,7 +7,7 @@ SECRET_KEY = os.environ['SECRET_KEY']
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['kubbuy-prod.herokuapp.com']
+ALLOWED_HOSTS = ['kubbuy-prod.fly.dev']
 
 DATABASES = {
     'default': dj_database_url.config()
