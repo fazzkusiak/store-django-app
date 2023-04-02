b1d7c5244109e2ccd1b2e133fc7033f2ffad99c1
kubusiaq <sactarek@gmail.com>
2023-04-02 13:14:11 +0200

    allowed host change

--- store/settings/prod.py
+++ store/settings/prod.py
@@ -7,7 +7,7 @@ SECRET_KEY = os.environ['SECRET_KEY']
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['kubbuy-prod.herokuapp.com']
+ALLOWED_HOSTS = ['kubbuy-prod.onrender.com']
 
 DATABASES = {
     'default': dj_database_url.config()
